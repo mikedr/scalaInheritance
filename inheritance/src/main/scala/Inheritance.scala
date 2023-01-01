7a07e700e0cba7abaa54f5f8bfbaf2d513c1732d
mikedr <derosamiguel@gmail.com>
2023-01-01 18:11:59 -0300

    Override

--- inheritance/src/main/scala/Inheritance.scala
+++ inheritance/src/main/scala/Inheritance.scala
@@ -1,19 +1,23 @@
 object Inheritance extends App {
 
   class Animal {
+    val creatureType: String = "Wild"
     def eat = println("Animal eating")
   }
 
   class Dog extends Animal {
+    override val creatureType: String = "Domestic"
     def eatDog= {
       eat
       println("Croquetas")
     }
+    override def eat = println("Como croquetas")
   }
 
   class Person( name: String, age: Int)
   class Employee(name: String, age: Int, idEmp: Int) extends Person(name, age)
 
   def dog = new Dog
-  dog.eatDog
+  dog.eat
+  println(dog.creatureType)
 }
